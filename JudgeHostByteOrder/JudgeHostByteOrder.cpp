90718b319709b9df0f61fab61263dbd52eb82891
84062594@qq.com <84062594@qq.com>
2019-09-18 13:38:38 +0800

    判断主机字节序

---
+++ JudgeHostByteOrder/JudgeHostByteOrder.cpp
@@ -0,0 +1,23 @@
+﻿// JudgeHostByteOrder.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <atlstr.h>
+#include <tchar.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+	DWORD dwSmallNum = 0x01020304;
+	if (dwSmallNum == htonl(dwSmallNum))
+	{
+		printf("Big Sequence.\r\n");
+	}
+	else
+	{
+		printf("Small Sequence.\r\n");
+	}
+	return 0;
+}
+
---
+++ JudgeHostByteOrder2/JudgeHostByteOrder2.cpp
@@ -0,0 +1,18 @@
+﻿// JudgeHostByteOrder2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <atlstr.h>
+int main()
+{
+	DWORD dwSmallNum = 0x01020304;
+	if ( * (BYTE *)&dwSmallNum == 0x04 )
+	{
+		printf("Small Sequence.\r\n");
+	}
+	else
+	{
+		printf("Big Sequence.\r\n");
+	}
+	return 0;
+}
