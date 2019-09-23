a04941f91996d72669dc1a28a0c09aaab7a886e9
84062594@qq.com <84062594@qq.com>
2019-09-23 17:40:30 +0800

    实验：a和b数值交换理解指针的含义

---
+++ ThePointVarible/ThePointVarible.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <tchar.h>
+#include "PointVArible.h"
+
+
+int _tmain(int argc,_TCHAR* argv[])
+{
+	int a = 5, b = 9;
+	int* pa=&a, * pb=&b;
+	swap(pa, pb);
+	std::cout << "a:" << a << "\t" << "b:" << b << std::endl;
+}
+
+void swap(int* pa, int* pb)
+{
+	int ptemp = *pa;
+	*pa = *pb;
+	*pb = ptemp;
+}
