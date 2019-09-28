c3029b6fb8f18ec8a8f0514df842e1df64a111a0
DubheStar <84062594@qq.com>
2019-09-28 23:12:36 +0800

    -

---
+++ Point/Point_Size/Point_Size/Point_Size.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using std::cout;
+
+int main()
+{
+	double a = 2.0;
+	double* p = &a;
+	std::cout << "Size of Point=" << sizeof(p);
+	std::cout << "Size of Point to Address's Value=" << sizeof(*p);
+}
