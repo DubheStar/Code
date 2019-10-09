9389daf0e19c60e950e4f1395baae301d5174e2d
84062594@qq.com <84062594@qq.com>
2019-10-09 18:43:00 +0800

    克隆账户检测

---
+++ CloneAccount/CloneAccount.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <tchar.h>
+using std::cout;
+
+int _tmain()
+{
+	HKEY key;
+	LPCTSTR subkey = _T("\\SAM\\SAM\\Domains\\Account\\Users\\000003EA");
+	if (ERROR_SUCCESS == (HKEY_LOCAL_MACHINE, subkey, 0, KEY_ALL_ACCESS, &key))
+	{
+		std:cout << "open success!";
+	}
+	system("pause");
+}
