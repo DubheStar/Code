8546eadd71da937e0a64460a3ac3121ae496e191
84062594@qq.com <84062594@qq.com>
2019-10-17 16:04:09 +0800

    使用C++的List容器实现端口扫描器

--- MyPortScaner/Scanner.cpp
+++ MyPortScaner/Scanner.cpp
@@ -51,8 +51,8 @@ void CScanner::Addip(char * szIP)
 		}
 
 		//解析IP地址字符串成IN_ADDR格式
-		char strStartIp[16];
-		char strEndIP[16];
+		char strStartIp[INET_ADDRLEN];
+		char strEndIP[INET_ADDRLEN];
 		DWORD start = htonl(inet_addr(szStartIP));
 		//DWORD start = inet_pton(AF_INET, szStartIP, &strStartIp);
 		DWORD end = htonl(inet_addr(szEndIP));
--- MyPortScaner/Scanner_main.cpp
+++ MyPortScaner/Scanner_main.cpp
@@ -8,12 +8,12 @@ int main()
 	std::cout << "欢迎使用DubheStar多线程扫描器 \r\n" << std::endl;
 	std::cout << "输入IP或IP段: \r\n" << std::endl;
 	char szIP[128];
-	scanf_s("%s", szIP, 128);
+	gets_s(szIP, 128);
 	//ip处理
 	scanner.Addip(szIP);
 	std::cout << "请输入端口或端口范围（用“-”连接）:\r\n" << std::endl;
 	char szPort[128];
-	scanf_s("%s", szPort, 128);
+	gets_s(szPort,128);
 	std::cout << std::endl;
 	//端口处理
 	scanner.AddPort(szPort);
