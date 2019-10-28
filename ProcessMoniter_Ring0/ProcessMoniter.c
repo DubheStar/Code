cdc388817288b03dc4275a929203691b32719fa2
84062594 <84062594@qq.com>
2019-10-28 18:37:01 +0800

    Windows进程、线程监控驱动-调试完成及添加注释

--- ProcessMoniter_Ring0/ProcessMoniter.c
+++ ProcessMoniter_Ring0/ProcessMoniter.c
@@ -1,4 +1,4 @@
-#include "ntddk.h"
+﻿#include "ntddk.h"
 #include "windef.h"
 #include "define.h"
 
@@ -7,29 +7,39 @@
 
 const WCHAR devLink[] = L"\\DosDevices\\MonitorProcess";
 const WCHAR devName[] = L"\\Device\\MonitorProcess";
-UNICODE_STRING          devNameUnicd;
-UNICODE_STRING          devLinkUnicd;
-PVOID                    gpEventObject = NULL;
+UNICODE_STRING           devNameUnicd;
+UNICODE_STRING           devLinkUnicd;
+PVOID                    gpEventObject = NULL;	// 与应用程序通信的 Event 对象
 ULONG                    ProcessNameOffset = 0;
-CHAR                    outBuf[255];
-BOOL                    g_bMainThread;
+CHAR                     outBuf[255];
+BOOL                     g_bMainThread;
 ULONG                    g_dwParentId;
 CHECKLIST                CheckList;
-ULONG                    BuildNumber;
-ULONG                    SYSTEMID;
-PWCHAR                    Version[VERSIONLEN];
+ULONG                    BuildNumber;	//系统版本号        
+ULONG                    SYSTEMID;      //System进程的ID
+PWCHAR                   Version[VERSIONLEN];
 
 NTSTATUS PsLookupProcessByProcessId(IN ULONG ulProcId, OUT PEPROCESS* pEProcess);
 
+
+/*
+通过PsGetCurrentProcess函数来获取当前调用驱动的进程的EPROCESS结构的地址.EPROCESS结构的0x174偏移处存放着进程名.
+思路如下:
+驱动程序的加载函数DriverEntry是运行在System进程中的．
+(1) 通过PsGetCurrentProcess可以获取System进程的内核EPROCESS结构的地址,
+(2) 从该地址开始寻找"System"字符串．
+(3) 找到了便是EPROCESS的进程名存放的偏移处, 得到进程名在EPROCESS结构的偏移后,
+(4) 进程调用驱动的时候, 就可以直接在该偏移处获取当前进程名．
+*/
 ULONG GetProcessNameOffset()
 {
 	PEPROCESS curproc;
-	int i;
+	int i; 
 
 	curproc = PsGetCurrentProcess();
 
 	for (i = 0; i < 3 * PAGE_SIZE; i++)
-	{
+	{	//如果当前进程
 		if (!strncmp(SYSNAME, (PCHAR)curproc + i, strlen(SYSNAME)))
 		{
 			return i;
@@ -53,15 +63,9 @@ NTSTATUS GetRegValue(PCWSTR RegPath, PCWSTR ValueName, PWCHAR Value)
 	RtlInitUnicodeString(&UnicodeRegPath, RegPath);
 	RtlInitUnicodeString(&UnicodeValueName, ValueName);
 
-	InitializeObjectAttributes(&ObjectAttributes,
-		&UnicodeRegPath,
-		OBJ_CASE_INSENSITIVE,
-		NULL,
-		NULL);
+	InitializeObjectAttributes(&ObjectAttributes, &UnicodeRegPath, OBJ_CASE_INSENSITIVE, NULL, NULL);
 
-	Status = ZwOpenKey(&KeyHandle,
-		KEY_ALL_ACCESS,
-		&ObjectAttributes);
+	Status = ZwOpenKey(&KeyHandle,KEY_ALL_ACCESS,&ObjectAttributes);
 	if (Status != STATUS_SUCCESS)
 	{
 		DbgPrint("zwopenkey error\n");
@@ -69,14 +73,8 @@ NTSTATUS GetRegValue(PCWSTR RegPath, PCWSTR ValueName, PWCHAR Value)
 	}
 
 	valueInfoLength = sizeof(KEY_VALUE_PARTIAL_INFORMATION) + VERSIONLEN;
-	valueInfoP = (PKEY_VALUE_PARTIAL_INFORMATION)ExAllocatePool
-	(NonPagedPool, valueInfoLength);
-	Status = ZwQueryValueKey(KeyHandle,
-		&UnicodeValueName,
-		KeyValuePartialInformation,
-		valueInfoP,
-		valueInfoLength,
-		&returnLength);
+	valueInfoP = (PKEY_VALUE_PARTIAL_INFORMATION)ExAllocatePool(NonPagedPool, valueInfoLength);
+	Status = ZwQueryValueKey(KeyHandle, &UnicodeValueName, KeyValuePartialInformation, valueInfoP, valueInfoLength, &returnLength);
 
 	if (!NT_SUCCESS(Status))
 	{
@@ -115,10 +113,7 @@ VOID ThreadCreateMon(IN HANDLE PId, IN HANDLE TId, IN BOOLEAN  bCreate)
 	if (bCreate)
 	{
 		dwParentPID = PsGetCurrentProcessId();
-		status = PsLookupProcessByProcessId(
-			(ULONG)dwParentPID,
-			&PEProcess);
-		if (!NT_SUCCESS(status))
+		status = PsLookupProcessByProcessId((ULONG)dwParentPID, &PEProcess); if (!NT_SUCCESS(status))
 		{
 			ObDereferenceObject(EProcess);
 			DbgPrint("error\n");
@@ -130,18 +125,14 @@ VOID ThreadCreateMon(IN HANDLE PId, IN HANDLE TId, IN BOOLEAN  bCreate)
 			ObDereferenceObject(EProcess);
 			return;
 		}
-		if ((g_bMainThread == TRUE)
-			&& (g_dwParentId != dwParentPID)
-			&& (dwParentPID != PId)
-			)
+		if ((g_bMainThread == TRUE) && (g_dwParentId != dwParentPID) && (dwParentPID != PId))
 		{
 			g_bMainThread = FALSE;
-			sprintf(outBuf, "r_thread|%s|%d|%s|%d\n",
-				(char*)((char*)EProcess + ProcessNameOffset),
-				PId,
-				(char*)((char*)PEProcess + ProcessNameOffset), dwParentPID);
+			sprintf(outBuf, "r_thread|%s|%d|%s|%d\n", (char*)((char*)EProcess + ProcessNameOffset), PId, (char*)((char*)PEProcess + ProcessNameOffset), dwParentPID);
 			if (gpEventObject != NULL)
+			{
 				KeSetEvent((PRKEVENT)gpEventObject, 0, FALSE);
+			}
 		}
 		if (CheckList.ONLYSHOWREMOTETHREAD)
 		{
@@ -149,14 +140,8 @@ VOID ThreadCreateMon(IN HANDLE PId, IN HANDLE TId, IN BOOLEAN  bCreate)
 			ObDereferenceObject(EProcess);
 			return;
 		}
-		DbgPrint("thread|%s|%d|%s|%d\n",
-			(char*)((char*)EProcess + ProcessNameOffset),
-			PId,
-			(char*)((char*)PEProcess + ProcessNameOffset), dwParentPID);
-		sprintf(outBuf, "thread|%s|%d|%s|%d\n",
-			(char*)((char*)EProcess + ProcessNameOffset),
-			PId,
-			(char*)((char*)PEProcess + ProcessNameOffset), dwParentPID);
+		DbgPrint("thread|%s|%d|%s|%d\n", (char*)((char*)EProcess + ProcessNameOffset), PId, (char*)((char*)PEProcess + ProcessNameOffset), dwParentPID);
+		sprintf(outBuf, "thread|%s|%d|%s|%d\n", (char*)((char*)EProcess + ProcessNameOffset), PId, (char*)((char*)PEProcess + ProcessNameOffset), dwParentPID);
 		if (gpEventObject != NULL)
 			KeSetEvent((PRKEVENT)gpEventObject, 0, FALSE);
 
@@ -324,6 +309,7 @@ NTSTATUS DeviceIoControlDispatch(
 	return status;
 }
 
+//入口函数(DriverEntry)：驱动的入口函数主要是对驱动程序进行初始化工作，它是由系统进程所调用。在驱动程序初始化的时候，入口函数被加载进内存，进行初始化，完成之后，就要退出内存。
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING theRegistryPath)
 {
 	NTSTATUS                Status;
@@ -331,12 +317,14 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING theRegi
 
 	DbgPrint("DriverEntry called!\n");
 	g_bMainThread = FALSE;
+	//清空outBuf数组,将数组的所有位都置为0
 	memset(outBuf, 0, 255);
-
+	//读取注册表键值并赋值到Version
 	if (1 != GetRegValue(L"\\Registry\\Machine\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"CSDVersion", Version))
 	{
 		DbgPrint("GetRegValueDword Wrong\n");
 	}
+	//获取操作系统版本号
 	PsGetVersion(NULL, NULL, &BuildNumber, NULL);
 	DbgPrint("[[[%d]]]:[[[%ws]]]", BuildNumber, Version);
 
@@ -358,18 +346,25 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING theRegi
 	}
 
 	ProcessNameOffset = GetProcessNameOffset();
+	//指定卸载例程函数
 	pDriverObject->DriverUnload = OnUnload;
+
+	//注册分发例程
 	pDriverObject->MajorFunction[IRP_MJ_CREATE] = pDriverObject->MajorFunction[IRP_MJ_CLOSE] = pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DeviceIoControlDispatch;
 
+	//注册Windows进程创建回调函数，当有进程创建时会调用ProcessCreateMon()回调函数
 	Status = PsSetCreateProcessNotifyRoutine(ProcessCreateMon, FALSE);
 	if (!NT_SUCCESS(Status))
 	{
+		//如果未调用成功则移除回调函数，PsSetCreateProcessNotifyRoutine第二参数为是否Remove，若为True，则移除，为False则注册。
 		DbgPrint("PsSetCreateProcessNotifyRoutine error\n");
 		goto CleanSymbolLink;
 	}
+	//注册Windows进程创建回调函数，当有线程创建时会调用ProcessCreateMon()回调函数
 	Status = PsSetCreateThreadNotifyRoutine(ThreadCreateMon);
 	if (!NT_SUCCESS(Status))
 	{
+		//如果未调用成功则移除回调函数，PsSetCreateThreadNotifyRoutine第二参数为是否Remove，若为True，则移除，为False则注册。
 		DbgPrint("PsSetCreateThreadNotifyRoutine error\n");
 		goto CleanProcessNotify;
 	}
