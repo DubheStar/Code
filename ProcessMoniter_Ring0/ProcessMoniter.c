ca755b775b8efa4a6870f508ca52204763d8e7b1
84062594 <84062594@qq.com>
2019-10-29 18:31:37 +0800

    添加注释

--- ProcessMoniter_Ring0/ProcessMoniter.c
+++ ProcessMoniter_Ring0/ProcessMoniter.c
@@ -23,7 +23,7 @@ NTSTATUS PsLookupProcessByProcessId(IN ULONG ulProcId, OUT PEPROCESS* pEProcess)
 
 
 /*
-通过PsGetCurrentProcess函数来获取当前调用驱动的进程的EPROCESS结构的地址.EPROCESS结构的0x174偏移处存放着进程名.
+通过PsGetCurrentProcess函数来获取当前调用驱动的进程的EPROCESS结构地址的0x174偏移处存放的进程名.
 思路如下:
 驱动程序的加载函数DriverEntry是运行在System进程中的．
 (1) 通过PsGetCurrentProcess可以获取System进程的内核EPROCESS结构的地址,
@@ -40,7 +40,7 @@ ULONG GetProcessNameOffset()
 
 	for (i = 0; i < 3 * PAGE_SIZE; i++)
 	{	//如果当前进程
-		if (!strncmp(SYSNAME, (PCHAR)curproc + i, strlen(SYSNAME)))
+		if (0 == strncmp(SYSNAME, (PCHAR)curproc + i, strlen(SYSNAME)))
 		{
 			return i;
 		}
@@ -119,7 +119,7 @@ VOID ThreadCreateMon(IN HANDLE PId, IN HANDLE TId, IN BOOLEAN  bCreate)
 			DbgPrint("error\n");
 			return;
 		}
-		if (PId == 4)
+		if (PId == 4)//System进程创建的不管
 		{
 			ObDereferenceObject(PEProcess);
 			ObDereferenceObject(EProcess);
@@ -134,7 +134,7 @@ VOID ThreadCreateMon(IN HANDLE PId, IN HANDLE TId, IN BOOLEAN  bCreate)
 				KeSetEvent((PRKEVENT)gpEventObject, 0, FALSE);
 			}
 		}
-		if (CheckList.ONLYSHOWREMOTETHREAD)
+		if (CheckList.ONLYSHOWREMOTETHREAD)//只显示远线程
 		{
 			ObDereferenceObject(PEProcess);
 			ObDereferenceObject(EProcess);
@@ -157,15 +157,21 @@ VOID ThreadCreateMon(IN HANDLE PId, IN HANDLE TId, IN BOOLEAN  bCreate)
 	ObDereferenceObject(EProcess);
 }
 
-
+//进程创建回调函数，PsSetCreateProcessNotifyRoutine第二参数为是否Remove，若为True，则移除，为False则注册。
+/*
+	void PcreateProcessNotifyRoutine(HANDLE ParentId, HANDLE ProcessId, BOOLEAN Create)
+	{
+	}
+*/
 VOID ProcessCreateMon(HANDLE hParentId, HANDLE PId, BOOLEAN bCreate)
 {
 
-	PEPROCESS        EProcess, PProcess;
+	PEPROCESS        EProcess, PProcess;//EPROCESS结构是一个不透明的结构，它充当进程的进程对象
 	NTSTATUS        status;
 	HANDLE            TId;
 
 	g_dwParentId = hParentId;
+	//Pid------ > Object(EPROCESS)原理就是通过 PsLookUpProcessByProcessId 传入PID.传出EPROCESS.
 	status = PsLookupProcessByProcessId((ULONG)PId, &EProcess);
 	if (!NT_SUCCESS(status))
 	{
@@ -176,38 +182,47 @@ VOID ProcessCreateMon(HANDLE hParentId, HANDLE PId, BOOLEAN bCreate)
 	if (!NT_SUCCESS(status))
 	{
 		DbgPrint("error\n");
-		ObDereferenceObject(EProcess);
+		ObDereferenceObject(EProcess);//对象创建(ObCreateObject)和对象删除(ObDereferenceObject、ObpRemoveObjectRoutine)
 		return;
 	}
 
-	if (bCreate)
+	if (bCreate)//如果为True，则为进程创建，反之则为退出
 	{
 		g_bMainThread = TRUE;
-		DbgPrint("process|%s|%d|%s|%d\n",
-			(char*)((char*)EProcess + ProcessNameOffset),
-			PId,
-			(char*)((char*)PProcess + ProcessNameOffset),
-			hParentId
-		);
-		sprintf(outBuf, "process|%s|%d|%s|%d\n",
-			(char*)((char*)EProcess + ProcessNameOffset),
-			PId,
-			(char*)((char*)PProcess + ProcessNameOffset),
-			hParentId
-		);
+		DbgPrint("process|%s|%d|%s|%d\n", (char*)((char*)EProcess + ProcessNameOffset), PId, (char*)((char*)PProcess + ProcessNameOffset), hParentId);
+		sprintf(outBuf, "process|%s|%d|%s|%d\n", (char*)((char*)EProcess + ProcessNameOffset), PId, (char*)((char*)PProcess + ProcessNameOffset), hParentId);
 		if (gpEventObject != NULL)
+		{
 			KeSetEvent((PRKEVENT)gpEventObject, 0, FALSE);
+		}
 	}
-	else if (CheckList.SHOWTERMINATEPROCESS)
+	else if (CheckList.SHOWTERMINATEPROCESS)//进程退出
 	{
 		DbgPrint("process_over|%d\n", PId);
 		sprintf(outBuf, "process_over|%d\n", PId);
 		if (gpEventObject != NULL)
+		{
 			KeSetEvent((PRKEVENT)gpEventObject, 0, FALSE);
+		}
 	}
 
-	ObDereferenceObject(PProcess);
+	ObDereferenceObject(PProcess);//删除对象
 	ObDereferenceObject(EProcess);
+}
+
+VOID ImageCreateMon(IN PUNICODE_STRING  FullImageName, IN HANDLE  ProcessId, IN PIMAGE_INFO  ImageInfo)
+{
+	do{
+		//判断系统进程
+		if (ProcessId == 4)break;
+		//对于新加载的内核模式组件（如驱动程序），请设置为一，对于映射到用户空间的映像，设置为零。
+		if (ImageInfo->SystemModeImage != TRUE)break;
+
+	} while (FALSE);
+
+	DbgPrint("FullImageName: %S,Process ID: %d\n", FullImageName->Buffer, ProcessId);
+	DbgPrint("ImageBase: %x,ImageSize: %d\n", ImageInfo->ImageBase, ImageInfo->ImageSize);
+	DbgPrint("ImageSignatureLevel:%d,ImageSignatureType:%d,SystemModeImage:%d", ImageInfo->ImageSignatureLevel, ImageInfo->ImageSignatureType, ImageInfo->SystemModeImage);
 }
 
 NTSTATUS OnUnload(IN PDRIVER_OBJECT pDriverObject)
@@ -263,7 +278,7 @@ NTSTATUS DeviceIoControlDispatch(
 		outputLength = irpStack->Parameters.DeviceIoControl.OutputBufferLength;
 		switch (irpStack->Parameters.DeviceIoControl.IoControlCode)
 		{
-		case IOCTL_PASSEVENT:
+		case IOCTL_PASSEVENT://用事件做通信
 			inputBuffer = pIrp->AssociatedIrp.SystemBuffer;
 
 			DbgPrint("inputBuffer:%08x\n", (HANDLE)inputBuffer);
@@ -322,29 +337,39 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING theRegi
 	//读取注册表键值并赋值到Version
 	if (1 != GetRegValue(L"\\Registry\\Machine\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"CSDVersion", Version))
 	{
-		DbgPrint("GetRegValueDword Wrong\n");
+		DbgPrint("GetRegValueDword Wrong\r\n");
 	}
 	//获取操作系统版本号
 	PsGetVersion(NULL, NULL, &BuildNumber, NULL);
 	DbgPrint("[[[%d]]]:[[[%ws]]]", BuildNumber, Version);
 
-	RtlInitUnicodeString(&devNameUnicd, devName);
+	RtlInitUnicodeString(&devNameUnicd, devName);//WCHAR转换为UNICODE_STRING结构体
 	RtlInitUnicodeString(&devLinkUnicd, devLink);
-
-	Status = IoCreateDevice(pDriverObject,0,&devNameUnicd,FILE_DEVICE_UNKNOWN,0,TRUE,&pDevice);
+	//创建设备
+	Status = IoCreateDevice(pDriverObject,//驱动程序对象
+		0,//扩展设备的大小，由于不需要，所以为0
+		&devNameUnicd,//设备名称
+		FILE_DEVICE_UNKNOWN,//设备类型
+		0,//指示设备允许的操作
+		TRUE,//若为TRUE，表示只能由一个线程使用该设备，反之则没有限制
+		&pDevice//返回的设备对象
+	);
 	if (!NT_SUCCESS(Status))
 	{
 		DbgPrint(("Can not create device.\n"));
 		goto out;
 	}
-
-	Status = IoCreateSymbolicLink(&devLinkUnicd, &devNameUnicd);
+	//创建符号链接
+	Status = IoCreateSymbolicLink(&devLinkUnicd,//存放符号链接的UNICODE_STRING
+		&devNameUnicd//设备名称
+	);
+	//如果创建符号链接失败则卸载驱动
 	if (!NT_SUCCESS(Status))
 	{
 		DbgPrint(("Cannot create link.\n"));
 		goto CleanDevice;
 	}
-
+	//通过PsGetCurrentProcess函数来获取当前调用驱动的进程的EPROCESS结构地址的0x174偏移处存放的进程名.
 	ProcessNameOffset = GetProcessNameOffset();
 	//指定卸载例程函数
 	pDriverObject->DriverUnload = OnUnload;
@@ -353,6 +378,10 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING theRegi
 	pDriverObject->MajorFunction[IRP_MJ_CREATE] = pDriverObject->MajorFunction[IRP_MJ_CLOSE] = pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DeviceIoControlDispatch;
 
 	//注册Windows进程创建回调函数，当有进程创建时会调用ProcessCreateMon()回调函数
+	/*
+	NTSTATUS PsSetCreateProcessNotifyRoutine(PCREATE_PROCESS_NOTIFY_ROUTINE NotifyRoutine, BOOLEAN Remove);
+	PCREATE_PROCESS_NOTIFY_ROUTINE PcreateProcessNotifyRoutine;
+	*/
 	Status = PsSetCreateProcessNotifyRoutine(ProcessCreateMon, FALSE);
 	if (!NT_SUCCESS(Status))
 	{
@@ -366,11 +395,20 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING theRegi
 	{
 		//如果未调用成功则移除回调函数，PsSetCreateThreadNotifyRoutine第二参数为是否Remove，若为True，则移除，为False则注册。
 		DbgPrint("PsSetCreateThreadNotifyRoutine error\n");
-		goto CleanProcessNotify;
+		goto CleanTreadNotify;
 	}
-
+	Status = PsSetLoadImageNotifyRoutine(ImageCreateMon);
+	if (!NT_SUCCESS(Status))
+	{
+		DbgPrint("PsSetLoadImageNotifyRoutine error\n");
+		goto CleanImageNotify;
+		//return Status;
+	}
 	return STATUS_SUCCESS;
-
+CleanTreadNotify:
+	PsRemoveCreateThreadNotifyRoutine(ThreadCreateMon);
+CleanImageNotify:
+	PsRemoveLoadImageNotifyRoutine(ImageCreateMon);
 CleanProcessNotify:
 	PsSetCreateProcessNotifyRoutine(ProcessCreateMon, TRUE);
 CleanSymbolLink:
